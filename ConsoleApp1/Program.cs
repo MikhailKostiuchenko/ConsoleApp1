cf4c44d3cce04379a1ea0fc0fb06c3a3a281ebae
Mikhail <mikhail@smarttouchinteractive.com>
2018-09-25 16:53:07 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic someObj = null;
+            int somevar;
+            try
+            {
+                somevar = (int)someObj;
+            }
+            catch (Exception e)
+            {
+                somevar = 25;
+            }
+            
+            Console.Write(somevar);
+        }
+    }
+}
