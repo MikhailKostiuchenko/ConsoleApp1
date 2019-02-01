336d6f48a3516018e223cd4ec31a0442fce31b74
Mikhail <mikhail@smarttouchinteractive.com>
2019-02-01 16:19:54 +0200

    commit of nb_1_9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,58 +8,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var mc = new MyClass_1();
-            int somevar;
-            try
-            {
-                Console.WriteLine("some new code");
-                Console.WriteLine("some new code");
-                Console.WriteLine("some new code");
-                Console.WriteLine("some new code");
-            }
-
-            // added by NB_1
-            catch (Exception e)
-            {
-            }
-
-            // added by NB_1
-            // added by NB_1
-            // added by NB_1
-            
-
+            // clear all
         }
-
-        //    NB_1
-        public int GetSquare(int number) => number * number;
-        
-        //    NB_1
-        public double GetSqrt(int number) => Math.Sqrt(number);
-    }
-
-    class MyClass_1
-    {
-        //    Created by NewBranch_1
-        void SomeMethod()
-        {
-
-        }
-
-    }
-
-    class MyClass2
-    {
-        // DELETED by NB_1
-        public string GetterSetter1 { get; set; }
-    }
-
-    class MyClass3
-    {
-        // ADDED by NB_1
-        public int Prop1 { get; set; }
-        public int Prop2 { get; set; }
-        public int Prop3 { get; set; }
-        public int Prop4 { get; set; }
-        
     }
 }    
