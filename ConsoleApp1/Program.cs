74cd448af50f9b70868c34679683ec641841cd64
Mikhail <mikhail@smarttouchinteractive.com>
2019-02-25 16:13:44 +0200

    some ch nb1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // clear all (nb1)
-            // some ch (nb1)
-            // clear all (nb2)
             // some ch (nb2)
         }
     }
