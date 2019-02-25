e14e6754e1a0e0b68d23dc8899781db574a34cf6
Mikhail <mikhail@smarttouchinteractive.com>
2019-02-25 16:08:59 +0200

    clear all

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // clear all
-            //add something
-            // add else
+            // clear all (nb2)
         }
     }
 }    
